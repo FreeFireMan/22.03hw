dcdfbe145774ad95f1271ba229b66ba27c0922c1
freefireman <beluh.c.s@gmail.com>
2021-03-23 14:53:08 +0200

    little fix 2))

--- src/components/main/userBlock/UserBlock.js
+++ src/components/main/userBlock/UserBlock.js
@@ -1,12 +1,10 @@
 import React from 'react'
 import './UserBlock.css'
 
-function UserBlock(props) {
-    let {src, name, aboutUser} = props;
-
+const UserBlock = ({src, name, aboutUser}) => {
     return (
         <div className={'userBlock'}>
-            <img src={src} />
+            <img src={src} alt='some text'/>
             <h2>{name}</h2>
             <p>{aboutUser}</p>
         </div>
